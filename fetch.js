22898e6703747b20ca18be9b0aa8f19b67b5840e
AMISHA S <amishasivakumar02@gmail.com>
2024-02-23 11:17:52 +0530

    23/02

--- fetch.js
+++ fetch.js
@@ -3,18 +3,18 @@ fetch('https://dummyjson.com/products/50')
     //.then((res) => console.log(res.products[0]))
     .then((res) => {
         console.log(res);
-        // let sum =0
-        // for (let i = 0; i < res.products.length; i++) {
-        //     // console.log(res.products[i].title, '\n', res.products[i].description, '\n', res.products[i].price);
-        //     // console.log(res.products[i].rating);
-        //     // if (res.products[i].rating >= 4) {
-        //     //     console.log("****");
-        //     // }
-        //     // console.log(res.products[i].price);
+        let sum =0
+        for (let i = 0; i < res.products.length; i++) {
+            console.log(res.products[i].title, '\n', res.products[i].description, '\n', res.products[i].price);
+            console.log(res.products[i].rating);
+            if (res.products[i].rating >= 4) {
+                console.log("****");
+            }
+            console.log(res.products[i].price);
             
-        //     sum += res.products[i].price
+            sum += res.products[i].price
             
-        // }
-        // console.log(sum);
+        }
+        console.log(sum);
     })
     .catch((err) => console.log(err))
