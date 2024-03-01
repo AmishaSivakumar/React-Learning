b8c8765b35ce49b626ecb33a1c781b93453d5742
AMISHA S <amishasivakumar02@gmail.com>
2024-03-01 18:57:28 +0530

    01/03

---
+++ destructuring.js
@@ -0,0 +1,16 @@
+// Object destructuring
+const data = {
+    username: "Amisha",
+    email: "abc@gamil.com",
+    mobile: 1234567890,
+    password: "1#Njdke35%nf",
+}
+
+const { username } = data;
+console.log(username);
+
+// Array destructuring
+
+const array = [24, 75, 83, 79]
+const [two, three, one] = array;
+console.log(three);
--- higherOrder.js
+++ higherOrder.js
@@ -12,15 +12,15 @@ const numbers = [23, 65, 78, 34, 89, 46, 84]
 // console.log(sum);
 
 // map ==>> get the array with same index  with manipulation
-// const result = numbers.map(x => x - 2)
-// console.log(result);
+   const result = numbers.map(x => x - 2)
+   console.log(result);
 
-// filter ==>> check the condition and return 
+// filter ==>> check the condition and return
 // const result = numbers.filter(x => x % 2 === 0)
 // console.log(result);
 
 //reduce
-const sum = numbers.reduce((acc, cur) => {
-    return acc += cur
-}, 0)
-console.log(sum);
+// const sum = numbers.reduce((acc, cur) => {
+//     return acc += cur
+// }, 0)
+// console.log(sum);
